0f579e9be2ad89c846e34a752dd10ebf10bcc7b9
0xspeedybird <0xspeedybird@gmail.com>
2024-12-20 10:02:31 -0500

    Add cover image to search results, if available.

--- apps/app/components/header/search.tsx
+++ apps/app/components/header/search.tsx
@@ -15,7 +15,40 @@ import track from "@/lib/track";
 import { fetchPipelines, fetchFeaturedPipelines } from "./fetchPipelines";
 import { LoaderCircleIcon } from "lucide-react";
 
-export default function Search({ pipeline, onPipelineSelect }: { pipeline?: any; onPipelineSelect?: (pipeline: any) => void }) {
+
+const PipelineCommand = ({ pipeline, onSelect, uniqueModifier = "unique" }: { pipeline: any; onSelect: (value: string) => void; uniqueModifier?: string }) => {
+  return (
+      <CommandItem value={`${pipeline.name}-${pipeline.id}-${uniqueModifier}`}
+                   key={`${pipeline.id}-${uniqueModifier}`}
+                   onSelect={() => onSelect(pipeline)}
+      >
+        <div className="flex items-center gap-2">
+          <div className="h-8 w-8 flex-shrink-0">
+            {pipeline.cover_image ? (
+                <img
+                    className="h-8 w-8 object-cover rounded"
+                    src={pipeline.cover_image}
+                    alt="pipeline"
+                    onError={(e) => {
+                      e.currentTarget.src = "";
+                      e.currentTarget.classList.add("hidden");
+                    }}
+                />
+            ) : (
+                <div className="h-full w-full bg-gray-200 rounded" aria-hidden="true"></div>
+            )}
+          </div>
+          <span>{pipeline.name}</span>
+        </div>
+      </CommandItem>
+);
+};
+
+export default function Search({
+  pipeline, onPipelineSelect
+}: {
+  pipeline ? : any;
+  onPipelineSelect?: (pipeline: any) => void }) {
   const [open, setOpen] = useState(false);
   const [selectedPipeline, setSelectedPipeline] = useState<any | null>(pipeline);
   const [query, setQuery] = useState("");
@@ -47,18 +80,30 @@ export default function Search({ pipeline, onPipelineSelect }: { pipeline?: any;
   };
 
   useEffect(() => {
+    // when query changes, this local var will
+    // prevent the result from a previous search
+    // from setting the results state (aka prevent race condition)
+    // so that results are in sync with the latest query.
+    let isCancelled = false;
+
     const fetchData = async () => {
       if (query.length >= 3) {
         setIsSearching(true);
         const data = await fetchPipelines(query);
-        setResults(data);
-        setIsSearching(false);
-      } else {
-        setResults([]);
+        console.log('isCancelled', isCancelled)
+        if (!isCancelled) {
+          console.log(data)
+          setIsSearching(false);
+          setResults(data);
+        }
       }
     };
 
     fetchData();
+
+    return () => {
+      isCancelled = true;
+    };
   }, [query]);
 
   return (
@@ -73,47 +118,39 @@ export default function Search({ pipeline, onPipelineSelect }: { pipeline?: any;
             }}
         />
         <CommandDialog open={open} onOpenChange={setOpen}>
-          <div className="flex justify-between items-center p-2">
-            <CommandInput
-                value={query}
-                onValueChange={(value) => setQuery(value)}
-                placeholder="Search Pipelines..."
-            />
-            <Button onClick={() => setOpen(false)}>Close</Button>
-          </div>
+          <CommandInput
+              value={query}
+              onValueChange={(value) => setQuery(value)}
+              placeholder="Search Pipelines..."
+          />
           <CommandList>
             {isSearching ? (
+              <CommandEmpty>
                 <div className="flex justify-center items-center py-4">
                   <LoaderCircleIcon className="w-8 h-8 animate-spin" />
                 </div>
+              </CommandEmpty>
             ) : query.length === 0 ? (
                 <CommandGroup heading="Featured Pipelines">
                   {featuredPipelines.map((pipeline: any) => (
-                      <CommandItem
-                          key={pipeline.id}
-                          onSelect={() => selectPipeline(pipeline)}
-                      >
-                        {pipeline.name}
-                      </CommandItem>
+                      <PipelineCommand key={`${pipeline.id}-command`} pipeline={pipeline} uniqueModifier={"featured"}
+                                       onSelect={() => selectPipeline(pipeline)}/>
                   ))}
                 </CommandGroup>
-            ) : results.length === 0 && query.length >= 3 ? (
-                <CommandEmpty>No results found.</CommandEmpty>
-            ) : results.length === 0 && query.length < 3 ? (
+            ) : query.length < 3 ? (
                 <CommandEmpty>Enter at least three characters to begin searching.</CommandEmpty>
+            ) : results.length === 0 ? (
+                <CommandEmpty>No results found.</CommandEmpty>
             ) : (
                 <CommandGroup heading="Search Results">
                   {results.map((pipeline: any) => (
-                      <CommandItem
-                          key={pipeline.id}
-                          onSelect={() => selectPipeline(pipeline)}
-                      >
-                        {pipeline.name}
-                      </CommandItem>
+                      <PipelineCommand key={`${pipeline.id}-command`} pipeline={pipeline}
+                                       onSelect={() => selectPipeline(pipeline)}/>
                   ))}
                 </CommandGroup>
             )}
           </CommandList>
+
         </CommandDialog>
       </div>
   );
