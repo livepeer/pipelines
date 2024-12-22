83686e1b4369f1c84f9d9cfcffd66a0547af6b89
Thom Shutt <thomshutt@users.noreply.github.com>
2024-12-22 10:16:19 +0000

    Revert "password protect (#78)"
    This reverts commit 469b3cf4e084595793ea17f6b4cf1a6a585f5ae3.

--- apps/app/app/stream/[id]/page.tsx
+++ apps/app/app/stream/[id]/page.tsx
@@ -58,16 +58,12 @@ export default function Stream({
   }, [fetchStream]);
 
   useEffect(() => {
-    const isVerified = localStorage.getItem('isVerified');
-    if (isVerified !== 'true') {
-      router.push('/password-protect');
-    }
     // Track when stream page is viewed
     track('create_stream_page_viewed', {
       stream_id: streamInputId,
       is_new: streamInputId === 'create'
     });
-  }, [streamInputId, router]);
+  }, [streamInputId]);
 
   const handleSaveStream = async () => {
     if (streamFormRef.current) {
