11aa212174ce1b6e92613d695c04064a1d05c64a
Thom Shutt <thomshutt@users.noreply.github.com>
2025-01-06 13:05:59 +0000

    Add types

--- apps/app/app/api/version/get.ts
+++ apps/app/app/api/version/get.ts
@@ -1,6 +1,8 @@
 "use server";
 
-export default function getVersion(req, res) {
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function getVersion(req: NextApiRequest, res: NextApiResponse) {
     res.status(200).json({
         gitSha: process.env.VERCEL_GIT_COMMIT_SHA || "unknown",
     });
