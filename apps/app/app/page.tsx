7ae8646939017d6ecc752f1a169e409707b6920a
Hunter Hillman <hunterhillman@hunters-mbp-109.lan>
2025-01-30 08:30:37 -0800

    missed import

--- apps/app/app/page.tsx
+++ apps/app/app/page.tsx
@@ -14,6 +14,7 @@ import { toast } from "sonner";
 import { Loader2 } from "lucide-react";
 import { upsertStream } from "@/app/api/streams/upsert";
 import { usePrivy } from "@privy-io/react-auth";
+import track from "@/lib/track";
 
 const App = ({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }): ReactElement => {
   const { user } = usePrivy();
