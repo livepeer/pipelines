027470bf371e376f8f62b37409d28be31a0597d7
Max Holland <max@hollandhome.co.uk>
2025-02-19 18:11:05 +0000

    Check env variable for direct playback (#197)
    * Check env var
    
    * Fix env var check
    
    * add comment

--- apps/app/components/playground/player.tsx
+++ apps/app/components/playground/player.tsx
@@ -28,10 +28,11 @@ export function LPPLayer({
   isMobile?: boolean;
   stream_key: string | null;
 }) {
+  // default to direct playback but allow us to disable this and go back to studio playback with an env variable or queryparam
+  let playerUrl = `https://ai.livepeer.${isProduction() ? "com" : "monster"}/aiWebrtc/${stream_key}-out`
   const searchParams = useSearchParams();
-  let playerUrl = `https://${isProduction() ? "lvpr.tv" : "monster.lvpr.tv"}/?v=${output_playback_id}&lowLatency=force&backoffMax=1000&ingestPlayback=true`
-  if (searchParams.get("directPlayback") === "true") {
-    playerUrl = `https://ai.livepeer.${isProduction() ? "com" : "monster"}/aiWebrtc/${stream_key}-out`
+  if ((searchParams.get("directPlayback") !== "true" && process.env.NEXT_PUBLIC_LIVEPEER_DIRECT_PLAYBACK === "false") || searchParams.get("directPlayback") === "false") {
+    playerUrl = `https://${isProduction() ? "lvpr.tv" : "monster.lvpr.tv"}/?v=${output_playback_id}&lowLatency=force&backoffMax=1000&ingestPlayback=true`
   }
   return (
     <div className={isMobile ? "w-full h-full" : "aspect-video"}>
