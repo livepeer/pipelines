d1e9e7e4c825a3d4d51f2d78d501636f4f41c1c0
Ashwin V C <ashwin@livepeer.org>
2025-02-21 22:58:41 +0530

    fix: hide email provider for mobile temporarily (#209)
    * fix: remove email provider for mobile temporarily
    
    * refactor to use privy based config

--- packages/design-system/providers/privy.tsx
+++ packages/design-system/providers/privy.tsx
@@ -1,20 +1,32 @@
 "use client";
 
 import { PrivyProvider as PrivyProviderRaw } from "@privy-io/react-auth";
+import { useIsMobile } from "../hooks/use-mobile";
 
-export const PrivyProvider = ({ children }: { children: React.ReactNode }) => (
-  <PrivyProviderRaw 
-    appId={"cm2xth5uy0acgf4dk18qfg2vq"}
-    config={{
-      appearance: {
-        theme: "dark",
-      },
-      legal: {
-        termsAndConditionsUrl: "https://www.livepeer.org/terms-of-service-p",
-        privacyPolicyUrl: "https://www.livepeer.org/privacy-policy-p",
-      },
-    }}
-  >
-    {children}
-  </PrivyProviderRaw>
-);
+const DEFAULT_LOGIN_METHODS = ["discord", "github", "wallet", "email"] as any;
+
+export const PrivyProvider = ({ children }: { children: React.ReactNode }) => {
+  const isMobile = useIsMobile();
+
+  const loginMethods = isMobile
+    ? DEFAULT_LOGIN_METHODS.filter((method: string) => method !== "email")
+    : DEFAULT_LOGIN_METHODS;
+
+  return (
+    <PrivyProviderRaw
+      appId={"cm2xth5uy0acgf4dk18qfg2vq"}
+      config={{
+        loginMethods,
+        appearance: {
+          theme: "dark",
+        },
+        legal: {
+          termsAndConditionsUrl: "https://www.livepeer.org/terms-of-service-p",
+          privacyPolicyUrl: "https://www.livepeer.org/privacy-policy-p",
+        },
+      }}
+    >
+      {children}
+    </PrivyProviderRaw>
+  );
+};
