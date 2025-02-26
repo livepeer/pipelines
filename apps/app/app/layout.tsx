25f9378fcab40aae9cdc34036d5b4bd40ff916cb
gioelecerati <gioele@cerati.org>
2025-02-26 23:28:03 +0100

    re-enable intercom

--- apps/app/app/layout.tsx
+++ apps/app/app/layout.tsx
@@ -44,7 +44,7 @@ const RootLayout = ({ children }: RootLayoutProperties) => (
               </div>
             </GlobalSidebar>
           </SidebarProvider>
-          {/* <Intercom /> */}
+          {<Intercom />}
           {/* TODO: REENABLE WHEN SHIH-YU IS READY FOR IT <Intercom /> */}
         </DesignSystemProvider>
       </MixpanelProvider>
