90c7239d5bc4121c3fc13887b42868cd23b991bf
Max Holland <max@livepeer.org>
2025-03-03 11:50:12 +0000

    Verify livepeer player works with mediamtx

--- apps/app/components/playground/player.tsx
+++ apps/app/components/playground/player.tsx
@@ -13,11 +13,17 @@ import * as Player from "@livepeer/react/player";
 import { CheckIcon, ChevronDownIcon, XIcon } from "lucide-react";
 import React, { useCallback, useTransition } from "react";
 import { toast } from "sonner";
+import { getSrc } from "@livepeer/react/external";
+import Livepeer from "livepeer";
 
 import { Src } from "@livepeer/react";
 import { cn } from "@repo/design-system/lib/utils";
 import { isProduction } from "@/lib/env";
 import { useSearchParams } from "next/navigation";
+
+// const livepeer = new Livepeer({
+//   apiKey: process.env.YOUR_PRIVATE_API_KEY,
+// });
 
 export function LPPLayer({
   output_playback_id,
@@ -29,23 +35,32 @@ export function LPPLayer({
   stream_key: string | null;
 }) {
   // default to direct playback but allow us to disable this and go back to studio playback with an env variable or queryparam
-  let playerUrl = `https://ai.livepeer.${isProduction() ? "com" : "monster"}/aiWebrtc/${stream_key}-out`;
+  let src = getSrc(`https://ai.livepeer.${isProduction() ? "com" : "monster"}/aiWebrtc/${stream_key}-out/whep`)
   const searchParams = useSearchParams();
   if (
     (searchParams.get("directPlayback") !== "true" &&
       process.env.NEXT_PUBLIC_LIVEPEER_DIRECT_PLAYBACK === "false") ||
     searchParams.get("directPlayback") === "false"
   ) {
-    playerUrl = `https://${isProduction() ? "lvpr.tv" : "monster.lvpr.tv"}/?v=${output_playback_id}&lowLatency=force&backoffMax=1000&ingestPlayback=true`;
+    // const playbackInfo = await livepeer.playback.get(output_playback_id);
+    // src = getSrc(playbackInfo.playbackInfo);
   }
   return (
-    <div className={isMobile ? "w-full h-full" : "aspect-video"}>
-      <iframe
-        src={playerUrl}
-        className="w-full h-full"
-        allow="fullscreen"
-        allowFullScreen
-      />
-    </div>
+    <Player.Root src={src} autoPlay={true}>
+      <Player.Container>
+        <Player.Video title="Live stream" />
+
+        <Player.Controls className="flex items-center justify-center">
+          <Player.PlayPauseTrigger className="w-10 h-10">
+            <Player.PlayingIndicator asChild matcher={false}>
+              <PlayIcon />
+            </Player.PlayingIndicator>
+            <Player.PlayingIndicator asChild>
+              <PauseIcon />
+            </Player.PlayingIndicator>
+          </Player.PlayPauseTrigger>
+        </Player.Controls>
+      </Player.Container>
+    </Player.Root>
   );
 }
