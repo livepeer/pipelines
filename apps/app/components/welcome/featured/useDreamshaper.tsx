212e7aecf60c2b6a5fb07367a13a461e9209a29b
Evan Mullins <evancmullins@gmail.com>
2025-03-04 10:37:27 -0600

    formatted

--- apps/app/components/welcome/featured/useDreamshaper.tsx
+++ apps/app/components/welcome/featured/useDreamshaper.tsx
@@ -304,7 +304,7 @@ export function useDreamshaper() {
         }
 
         const updatedInputValues = JSON.parse(JSON.stringify(inputValues));
-        
+
         const { cleanedPrompt, commands } = extractCommands(prompt);
 
         if (updatedInputValues?.prompt?.["5"]?.inputs) {
@@ -360,12 +360,14 @@ export function useDreamshaper() {
                   numValue = param.widgetConfig.max;
                 }
 
-                updatedInputValues.prompt[param.nodeId].inputs[actualField] = numValue;
+                updatedInputValues.prompt[param.nodeId].inputs[actualField] =
+                  numValue;
               } else if (param.widget === "checkbox") {
                 updatedInputValues.prompt[param.nodeId].inputs[actualField] =
                   value.toLowerCase() === "true" || value === "1";
               } else {
-                updatedInputValues.prompt[param.nodeId].inputs[actualField] = value;
+                updatedInputValues.prompt[param.nodeId].inputs[actualField] =
+                  value;
               }
             }
           });
