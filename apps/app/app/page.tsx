09db376da4055e4363c9f870ed900f4926053b25
Ashwin V C <ashwin@livepeer.org>
2025-03-07 00:45:37 +0530

    display sharedPrompts as OG tags description (#249)

--- apps/app/app/page.tsx
+++ apps/app/app/page.tsx
@@ -1,9 +1,33 @@
-import { Metadata } from "next";
 import DayDreamContent from "@/components/daydream";
+import { getSharedParams } from "./api/streams/share-params";
+import { cache } from "react";
 
-export const metadata: Metadata = {
-  title: "Daydream",
-  description: "Transform your video in real-time with AI",
+const getCachedSharedParams = cache(async (shareParamsId: string) => {
+  const { data: sharedParams } = await getSharedParams(shareParamsId);
+  return sharedParams;
+});
+
+export const generateMetadata = async ({
+  searchParams,
+}: {
+  searchParams: { shared: string };
+}) => {
+  const { shared } = searchParams;
+  const metaData = {
+    title: "Daydream",
+    description: "Transform your video in real-time with AI",
+  };
+
+  if (!shared) {
+    return metaData;
+  }
+
+  // Update the description with the shared prompt
+  const sharedParams = await getCachedSharedParams(shared);
+  const sharedPrompt =
+    sharedParams?.params?.prompt?.["5"]?.inputs?.text || metaData.description;
+  metaData.description = sharedPrompt;
+  return { ...metaData, openGraph: metaData };
 };
 
 export default function HomePage() {
