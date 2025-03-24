5935a07384bba90f66e71ac39468f67041dff320
Ashwin V C <ashwin@livepeer.org>
2025-03-24 20:15:32 +0530

    Revert vercel toolbar back to pre-prod envs

--- packages/design-system/providers/index.tsx
+++ packages/design-system/providers/index.tsx
@@ -3,6 +3,8 @@ import { Toaster } from "../components/ui/sonner";
 import { TooltipProvider } from "../components/ui/tooltip";
 import { PrivyProvider } from "./privy";
 import { ThemeProvider } from "./theme";
+// @ts-ignore
+import { VercelToolbar } from "@vercel/toolbar/next";
 type DesignSystemProviderProperties = ThemeProviderProps;
 
 export const DesignSystemProvider = ({
@@ -14,5 +16,6 @@ export const DesignSystemProvider = ({
       <TooltipProvider>{children}</TooltipProvider>
     </PrivyProvider>
     <Toaster />
+    {process.env.NODE_ENV !== "production" && <VercelToolbar />}
   </ThemeProvider>
 );
