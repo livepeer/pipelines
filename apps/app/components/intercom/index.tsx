3008833a758611edaae6811caffda9eaa016a27a
JunHyoung <junhyoung@livepeer.org>
2025-03-24 21:18:01 +0900

    feat: improve ui for mobile view (#304)
    * feat: enable intercom in mobile view
    
    * feat: display join community button in mobile view

--- apps/app/components/intercom/index.tsx
+++ apps/app/components/intercom/index.tsx
@@ -1,20 +1,15 @@
 "use client";
 
-import { useIsMobile } from "@repo/design-system/hooks/use-mobile";
 import { useEffect } from "react";
 
 export default function Intercom() {
-  const isMobile = useIsMobile();
-
   useEffect(() => {
-    if (!isMobile) {
-      import("@intercom/messenger-js-sdk").then(IntercomClient => {
-        IntercomClient.default({
-          app_id: process.env.NEXT_PUBLIC_INTERCOM_APP_ID || "",
-        });
+    import("@intercom/messenger-js-sdk").then(IntercomClient => {
+      IntercomClient.default({
+        app_id: process.env.NEXT_PUBLIC_INTERCOM_APP_ID || "",
       });
-    }
-  }, [isMobile]);
+    });
+  }, []);
 
   return null;
 }
