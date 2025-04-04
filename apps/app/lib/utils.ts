f1beae0d57ebf541b8352e9fa1461c06098eee96
gioelecerati <50955448+gioelecerati@users.noreply.github.com>
2025-04-04 17:19:16 +0200

    dd: fix: debug panel admin + helper (#351)
    * dd: fix: debug panel admin + helper
    
    * address comments

--- apps/app/lib/utils.ts
+++ apps/app/lib/utils.ts
@@ -1,2 +1,14 @@
+import { User } from "@privy-io/react-auth";
+
 export const sleep = (ms: number) =>
   new Promise(resolve => setTimeout(resolve, ms));
+
+export const isLivepeerEmail = (user: User | null | undefined): boolean => {
+  if (!user) return false;
+  
+  const email = user.email?.address || user.google?.email || user.discord?.email;
+
+  if (!email) return false;
+  
+  return email.endsWith("@livepeer.org");
+};
