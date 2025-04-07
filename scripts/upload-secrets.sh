2d6ae51da09ad79f2d467ac8adb5767926924b5a
junhyoung-ryu <junhyoung@livepeer.org>
2025-04-07 20:20:41 +0900

    ci: sanitize dotenv in script

--- scripts/upload-secrets.sh
+++ scripts/upload-secrets.sh
@@ -2,10 +2,19 @@
 
 REPO="livepeer/pipelines"
 
+# for secret in $(gh secret list --repo "$REPO" --json name -q '.[].name'); do
+#     echo "Deleting secret: $secret"
+#     gh secret delete "$secret" --repo "$REPO"
+# done
+
 while IFS='=' read -r key value
 do
-  if [[ -n "$key" && -n "$value" ]]; then
-    echo "Uploading $key..."
-    gh secret set "$key" --body "$value" --repo "$REPO"
+  clean_value=$(echo "$value" | sed -e 's/^"//' -e 's/"$//' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+
+  if [[ -n "$clean_value" ]]; then
+    echo "Uploading secret: $key"
+    gh secret set "$key" --body "$clean_value" --repo "$REPO"
+  else
+    echo "Skipping $key (empty value)"
   fi
 done < apps/app/.env
