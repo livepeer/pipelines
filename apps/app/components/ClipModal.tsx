30b23b8eca34b2f6c283d8c51f2c271d2bc6b008
Ashwin V C <ashwin@livepeer.org>
2025-04-22 18:24:05 +0530

    feat: add new sharing UI screens (#436)
    * add share clip and clip summary modals
    
    * fix margins, widths and add rotation check support
    
    * remove leaderboard toggle, until its hooked up
    
    * resolve comments

--- apps/app/components/ClipModal.tsx
+++ apps/app/components/ClipModal.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@repo/design-system/components/ui/dialog";
 import { Button } from "@repo/design-system/components/ui/button";
-import { Download } from "lucide-react";
+import { Separator } from "@repo/design-system/components/ui/separator";
+import { usePhoneRotation } from "@/hooks/usePhoneRotation";
 
 interface ClipModalProps {
   isOpen: boolean;
@@ -15,12 +17,20 @@ interface ClipModalProps {
   clipFilename: string | null;
 }
 
+/**
+ * TODOS:
+ * 1. Rename button from Download to Continue once the APIs are ready for clip sharing
+ * 2. Enable Switch toggle and upload logic to post to leaderboard
+ * 3. Add a button to share the clip to the leaderboard
+ */
 export function ClipModal({
   isOpen,
   onClose,
   clipUrl,
   clipFilename,
 }: ClipModalProps) {
+  const isRotating = usePhoneRotation();
+
   const handleDownload = () => {
     if (clipUrl && clipFilename) {
       const downloadLink = document.createElement("a");
@@ -35,14 +45,26 @@ export function ClipModal({
     }
   };
 
+  const handleOpenChange = (open: boolean) => {
+    if (!open && !isRotating) {
+      onClose();
+    }
+  };
+
   return (
-    <Dialog open={isOpen} onOpenChange={open => !open && onClose()}>
-      <DialogContent className="max-w-[calc(100%-2rem)] mx-auto sm:max-w-[600px] max-h-[80vh] overflow-y-auto rounded-xl">
-        <DialogHeader>
-          <DialogTitle>Your Clip is ready!</DialogTitle>
+    <Dialog open={isOpen} onOpenChange={handleOpenChange}>
+      <DialogContent className="h-fit max-h-[90dvh] max-w-[55dvh] mx-auto overflow-y-auto rounded-xl">
+        <DialogHeader className="flex items-center">
+          <DialogTitle className="text-2xl">Clip Summary</DialogTitle>
+          <DialogDescription className="font-light text-center">
+            Post the clip to Daydream leaderboard, download or share with your
+            friends
+          </DialogDescription>
         </DialogHeader>
 
-        <div className="mt-4 mb-4 flex justify-center">
+        <Separator className="my-2" />
+
+        <div className="flex justify-center">
           {clipUrl && (
             <video
               src={clipUrl}
@@ -51,18 +73,42 @@ export function ClipModal({
               muted={false}
               playsInline
               controls
-              className="w-full max-h-[60vh] object-contain rounded-md"
+              className="w-full sm:h-[50dvh] aspect-square rounded-md"
             />
           )}
         </div>
 
-        <div className="w-full mt-2">
+        {/* <div className="flex items-center justify-between mt-2">
+          <div className="flex flex-col items-start">
+            <div className="text-sm font-medium">Post to Leaderboard</div>
+            <div className="text-sm text-muted-foreground font-light">
+              This clip will be displayed in Daydream Leaderboard
+            </div>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <TooltipProvider delayDuration={0}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <InfoIcon className="w-4 h-4 hover:cursor-pointer" />
+                </TooltipTrigger>
+                <TooltipContent>
+                  This feature is not available yet. We shall roll out soon!
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <Switch disabled defaultChecked={false} />
+          </div>
+        </div> */}
+
+        <Separator className="my-2" />
+
+        <div className="w-full">
           <Button
             onClick={handleDownload}
-            className="w-full flex items-center justify-center gap-2 rounded-md"
+            className="w-full flex items-center justify-center gap-2 rounded-md h-[46px]"
           >
-            <Download className="h-4 w-4" />
-            Download Clip
+            Download
           </Button>
         </div>
       </DialogContent>
