8db3f00e1e3ddfcbab0a1e487fe28b4cbf4fda63
Hunter Hillman <hunterhillman@hunters-mbp-109.lan>
2025-04-24 12:34:32 -0700

    move to center

--- apps/app/components/welcome/featured/Dreamshaper.tsx
+++ apps/app/components/welcome/featured/Dreamshaper.tsx
@@ -358,7 +358,6 @@ export default function Dreamshaper({ isGuestMode = false }: DreamshaperProps) {
             <InputPrompt onPromptSubmit={handleGuestPromptSubmit} />
           )}
           <StreamDebugPanel />
-          <StreamInfo />
         </div>
       </div>
 
