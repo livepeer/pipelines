ed15591077cab883024d935cbb241f7caf3a7ef4
Ashwin V C <ashwin@livepeer.org>
2025-04-30 14:44:53 +0530

    feat: add 3P Analytics to Prod  (#470)
    * feat: add google analytics to prod env
    
    * refactor
    
    * remove unused <head> tag
    
    * add config to run only on Prod

---
+++ apps/app/scripts/analytics.tsx
@@ -0,0 +1,49 @@
+import Script from "next/script";
+import { getAppConfig } from "../lib/env";
+
+export const ThirdPartyAnalytics = () => {
+  const appConfig = getAppConfig();
+
+  if (appConfig.environment !== "production") {
+    return null;
+  }
+
+  return (
+    <>
+      {/* Hotjar */}
+      <Script
+        id="contentsquare"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              (function (c, s, q, u, a, r, e) {
+                  c.hj=c.hj||function(){(c.hj.q=c.hj.q||[]).push(arguments)};
+                  c._hjSettings = { hjid: a };
+                  r = s.getElementsByTagName('head')[0];
+                  e = s.createElement('script'); e.async = true; e.src = q + c._hjSettings.hjid + u; r.appendChild(e);
+              })(window, document, 'https://static.hj.contentsquare.net/c/csq-', '.js', ${process.env.NEXT_PUBLIC_HOTJAR_ID});
+            `,
+        }}
+      />
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+        }}
+      />
+    </>
+  );
+};
