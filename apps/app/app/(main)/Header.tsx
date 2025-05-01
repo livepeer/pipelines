51e79b2145e54e09cecbeea9bbbcb4832ae8e9eb
Hunter Hillman <hunterhillman@hunters-mbp-109.lan>
2025-05-01 12:31:52 -0700

    beta tag

--- apps/app/app/(main)/Header.tsx
+++ apps/app/app/(main)/Header.tsx
@@ -54,11 +54,16 @@ export default function Header() {
           aria-label="Global"
           className="mx-auto flex items-center justify-between py-4 px-4 sm:px-6 lg:px-8"
         >
-          <div className="flex flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
+          <div className="flex flex-1 items-center gap-3">
+            <a href="#" className="-m-1.5 p-1.5 flex items-center">
               <span className="sr-only">Daydream by Livepeer</span>
               <Logo />
             </a>
+            {/* Beta badge */}
+            <div className="flex items-center px-3 py-0.5 rounded-full border border-blue-200 bg-white/70 backdrop-blur-sm text-blue-600 text-sm font-medium gap-1 shadow-sm">
+              <span className="inline-block w-2 h-2 rounded-full bg-blue-500 mr-1"></span>
+              Beta
+            </div>
           </div>
           <div className="flex flex-1 justify-end">
             <TrackedButton
