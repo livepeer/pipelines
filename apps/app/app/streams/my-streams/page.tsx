a3a864e991ddeb3eaa21655c56873e962d120c3d
JunHyoung <junhyoung@livepeer.org>
2025-05-09 10:18:47 +0900

    fix: invalid type

--- apps/app/app/streams/my-streams/page.tsx
+++ apps/app/app/streams/my-streams/page.tsx
@@ -3,7 +3,7 @@ import MyStreamsClient from "./MyStreamsClient";
 export default async function MyStreams({
   searchParams,
 }: {
-  searchParams: Promise<{ searchParams: any }>;
+  searchParams: Promise<any>;
 }) {
   return <MyStreamsClient searchParams={await searchParams} />;
 }
