a9c342f1d6efa8fcfb9d4911274902e31a172230
Ashwin V C <ashwin@livepeer.org>
2025-05-16 21:25:00 +0530

    feat: add referrer to social links (#617)
    * add referrer to social links
    
    * send utm_source instead of referrer

--- apps/app/next.config.mjs
+++ apps/app/next.config.mjs
@@ -64,7 +64,7 @@ const nextConfig = {
     return [
       {
         source: "/:platform(x|tt|tiktok|twitter|ig|instagram|fb|facebook)",
-        destination: "/",
+        destination: "/?utm_source=:platform",
         permanent: false,
       },
     ];
