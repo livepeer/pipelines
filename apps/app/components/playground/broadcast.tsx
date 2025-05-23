0d000780496d6b38540ab17f03718ba972134d92
Jun <junhyoung@livepeer.org>
2025-05-23 20:07:36 +0900

    feat: enable audio (#691)
    * feat: enable full audio
    
    * chore: adjust audio button position

--- apps/app/components/playground/broadcast.tsx
+++ apps/app/components/playground/broadcast.tsx
@@ -289,7 +289,17 @@ export function BroadcastWithControls({ className }: { className?: string }) {
                     </Broadcast.VideoEnabledIndicator>
                   </Broadcast.VideoEnabledTrigger>
                 </div>
+
                 <div className="flex sm:flex-1 md:flex-[1.5] justify-end items-center gap-2.5">
+                  <Broadcast.AudioEnabledTrigger className="w-6 h-6 hover:scale-110 transition flex-shrink-0">
+                    <Broadcast.AudioEnabledIndicator asChild matcher={false}>
+                      <DisableAudioIcon className="w-full h-full text-white/50" />
+                    </Broadcast.AudioEnabledIndicator>
+                    <Broadcast.AudioEnabledIndicator asChild matcher={true}>
+                      <EnableAudioIcon className="w-full h-full text-white/50" />
+                    </Broadcast.AudioEnabledIndicator>
+                  </Broadcast.AudioEnabledTrigger>
+
                   <CameraSwitchButton />
 
                   <Broadcast.ScreenshareTrigger className="w-6 h-6 hover:scale-110 transition flex-shrink-0">
