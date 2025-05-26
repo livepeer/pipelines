a712d17b971bfb1906537d16a0656fdd4dc832c2
junhyr <junhyoung@livepeer.org>
2025-05-26 23:22:16 +0900

    feat: lower quality

--- apps/restreamers/restream-yt-to-lp.sh
+++ apps/restreamers/restream-yt-to-lp.sh
@@ -17,9 +17,9 @@ DOWNLOAD_DIR="/app/data" # Define download directory
 COOKIES_FILE="/app/cookies.txt"
 
 USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-FORMAT_SELECTOR="bestvideo[vcodec^=avc][ext=mp4]+bestaudio[acodec=aac][ext=m4a]/bestvideo[vcodec^=avc][ext=mp4]+bestaudio/bestvideo[ext=mp4]+bestaudio/best[ext=mp4]/best"
+FORMAT_SELECTOR="bestvideo[height<=480][vcodec^=avc][ext=mp4]+bestaudio[acodec=aac][ext=m4a]/bestvideo[height<=480][vcodec^=avc][ext=mp4]+bestaudio/bestvideo[height<=480][ext=mp4]+bestaudio/best[ext=mp4][height<=480]/best[height<=480]"
 FFMPEG_INPUT_OPTS="-re"
-FFMPEG_CODEC_OPTS="-c:v libx264 -preset veryfast -tune zerolatency -g 60 -keyint_min 60 -c:a aac -ar 44100 -b:a 128k"
+FFMPEG_CODEC_OPTS="-c copy"
 FFMPEG_OUTPUT_OPTS="-f flv"
 RESTART_DELAY="10"
 
