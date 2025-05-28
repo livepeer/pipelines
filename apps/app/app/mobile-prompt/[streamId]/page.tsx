32de5adf827fc14b0ad1f54da19c5e9a09ce0fb6
Hunter Hillman <hunterhillman@hunters-mbp-109.lan>
2025-05-28 11:58:31 -0700

    move to correct route

---
+++ apps/app/app/mobile-prompt/[streamId]/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+import { MobilePromptPanel } from "@/components/stream/MobilePromptPanel";
+
+export default function MobilePromptPage({
+  params,
+}: {
+  params: { streamId: string };
+}) {
+  return (
+    <div className="h-screen w-screen">
+      <MobilePromptPanel streamId={params.streamId} />
+    </div>
+  );
+}
