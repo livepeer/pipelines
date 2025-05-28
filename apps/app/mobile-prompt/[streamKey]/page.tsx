844ab24a013d0cdac3b629bf7eddb4c6f361b2d2
Hunter Hillman <hunterhillman@hunters-mbp-109.lan>
2025-05-28 10:25:54 -0700

    mobile QR

---
+++ apps/app/mobile-prompt/[streamKey]/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+import { MobilePromptPanel } from "@/components/stream/MobilePromptPanel";
+
+export default function MobilePromptPage({
+  params,
+}: {
+  params: { streamKey: string };
+}) {
+  return (
+    <div className="h-screen w-screen">
+      <MobilePromptPanel streamKey={params.streamKey} />
+    </div>
+  );
+} 
