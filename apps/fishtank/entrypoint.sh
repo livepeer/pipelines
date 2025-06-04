4819cfb94926961eee39b79c0374be595da7c7c4
Jun <junhyoung@livepeer.org>
2025-06-04 11:39:18 +0900

    feat: update rtmp target (#762)

--- apps/fishtank/entrypoint.sh
+++ apps/fishtank/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 STREAM_URL="https://storage.googleapis.com/thom-vod-testing/fishtank_director.mp4"
-RTMP_TARGET="rtmp://ai.livepeer.com/stk_*****"
+RTMP_TARGET="rtmp://ai.livepeer.monster/stk_m7QQNZPjUp5wP3NV"
 
 while true; do
-  ffmpeg -rw_timeout 10000000 -timeout 10000000 -re -i "$STREAM_URL" -c copy -f flv "$RTMP_TARGET"
+  ffmpeg -rw_timeout 10000000 -timeout 10000000 -an -re -i "$STREAM_URL" -c copy -f flv "$RTMP_TARGET"
   sleep 5
 done 
